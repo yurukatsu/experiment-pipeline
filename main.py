975b2b33517c321a12e5fbba79ccca4651ce35ec
yurukatsu <yuruyuru.kaihatsu@gmail.com>
2025-08-27 07:14:34 +0900

    format

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import luigi
 from src.tasks.train import TrainTask
 
 # Luigi UIに表示されるようにWorkerモードで実行
-if __name__ == '__main__':
+if __name__ == "__main__":
     settings_file_path = "configs/settings.yml"
 
     # Luigi UIで確認可能な方法で実行
-    luigi.build([
-        TrainTask(settings_file_path=settings_file_path)
-    ],
-    scheduler_host='localhost',
-    scheduler_port=8082,
-    workers=1
+    luigi.build(
+        [TrainTask(settings_file_path=settings_file_path)],
+        scheduler_host="localhost",
+        scheduler_port=8082,
+        workers=1,
     )
